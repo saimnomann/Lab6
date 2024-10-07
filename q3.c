6c18b358c9a4348c1c67c523eb94c2993584efb6
saimnomann <141673697+saimnomann@users.noreply.github.com>
2024-10-08 01:09:30 +0500

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+	int i,num,x=0,y=1,sum=0;
+	printf("Please enter number");
+	scanf("%d",&num);//5
+	for(i=1;i<=num;i++){
+		if(num == 1 ){
+			printf("It is neither prime nor composite\n");
+			break;
+		}
+		else if(num%i ==0 && i!=1 && i!=num ){
+			printf("It is not prime\n");
+			break;
+		}
+		else if(i==num) {
+			
+			printf("It is prime\n");	
+		}
+}
+for(i=0;i<num;i++){
+printf("%d,",sum);
+sum=x+y;
+x=y;
+y=sum;
+	}
+}
